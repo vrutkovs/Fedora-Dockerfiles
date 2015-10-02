e9ef2dbf577c0e558cebc2e86d9e00338f066b32
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-10-02 15:02:50 +0200

    apache: make sure /etc/httpd/logs is a dir

--- apache/run-apache.sh
+++ apache/run-apache.sh
@@ -4,5 +4,8 @@
 # context after restarting the container.  httpd won't start correctly
 # if it thinks it is already running.
 rm -rf /run/httpd/* /tmp/httpd*
+# /etc/httpd/logs is a file?
+rm -rf /etc/httpd/logs
+mkdir -p /etc/httpd/logs
 
 exec /usr/sbin/httpd -D FOREGROUND
