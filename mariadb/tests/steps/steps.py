a0592e282a11229f6e3cd03c5d2fe71765e57fb3
Vadim Rutkovsky <vrutkovs@redhat.com>
2015-10-09 15:18:51 +0200

    mysql: mariadb: connect via IP

--- mariadb/tests/steps/steps.py
+++ mariadb/tests/steps/steps.py
@@ -9,8 +9,7 @@ from random import randint
 @when(u'mariadb container is started')
 def mariadb_container_is_started(context):
     # Read mariadb params from context var
-    context.container_id = u'ctf%s' % randint(1, 10)
-    params = ' --name=%s --privileged=true' % context.container_id
+    params = ' --name=ctf --privileged=true'
     for param in context.mariadb:
         params += ' -e %s=%s' % (param, context.mariadb[param])
     context.execute_steps(u'* Docker container is started with params "%s"' % params)
@@ -48,8 +47,8 @@ def mariadb_connect(context, negative=False):
 
     for attempts in xrange(0, 5):
         try:
-            context.run('docker run --rm -i --volumes-from=%s %s mysql -u"%s" -p"%s" -e "SELECT 1;" %s' % (
-                context.container_id, context.image, user, password, db))
+            context.run('docker run --rm %s mysql -h %s -u"%s" -p"%s" -e "SELECT 1;" %s' % (
+                context.image, context.ip, user, password, db))
             return
         except AssertionError:
             # If  negative part was set, then we expect a bad code
